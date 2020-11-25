809b953764efbc7629524d060d5eb8a2dd9c0b2d
Viktor Gorokhov <patronusv@gmail.com>
2020-11-25 15:20:08 +0200

    добавляет решение 1-го задания

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,11 @@
 'use strict';
+const categoriesRef = document.querySelector('#categories');
+const categoriesCount = categories.children.length;
+console.log(`В списке ${categoriesCount} категории`);
+
+const itemRef = document.querySelectorAll('.item');
+const itemContents = itemRef.forEach(item => {
+  const itemHeading = item.querySelector('h2').textContent;
+  const itemCategoriesCount = item.querySelectorAll('li').length;
+  console.log(`Категория: ${itemHeading}\nКоличество элементов: ${itemCategoriesCount}`);
+});
