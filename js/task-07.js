85802ddbca3394e0fb38f02055ca32eeb2347b61
Viktor Gorokhov <patronusv@gmail.com>
2020-11-25 23:02:22 +0200

    добавляет решение 7-го задания

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,10 @@
 'use strict';
+const inputRef = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
+textRef.style.fontSize = `${inputRef.value}px`;
+const fontSize = () => {
+  textRef.style.fontSize = `${inputRef.value}px`;
+  console.log(Number(inputRef.value));
+};
+inputRef.addEventListener('input', fontSize); // плавное изменение, возможно более ресурсоёмко
+// inputRef.addEventListener('click', fontSize); // не плавное изменение, возможно менее ресурсоёмко
