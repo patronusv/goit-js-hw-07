9461fca0b55b6cb1a7d14b1de469386c06ec087e
Viktor Gorokhov <patronusv@gmail.com>
2020-11-25 16:34:57 +0200

    добавляет решение 2-го задания

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,9 @@
 'use strict';
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+
+const array = ingredients.forEach((item, index) => {
+  item = document.createElement('li');
+  item.textContent = ingredients[index];
+  const list = document.querySelector('#ingredients');
+  list.appendChild(item);
+});
