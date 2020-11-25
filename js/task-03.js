04111df03e157092995ff2bf131924a0e94da873
Viktor Gorokhov <patronusv@gmail.com>
2020-11-25 20:02:22 +0200

    добавляет решение 3-го задания

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,21 @@
 'use strict';
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+const list = document.querySelector('#gallery');
+
+const listRef = images.forEach(({ url, alt }) => {
+  list.insertAdjacentHTML('afterbegin', `<li><img src = "${url}" alt = "${alt}" height = "250"></li>`);
+});
+const imageRef = document.querySelectorAll('.image');
