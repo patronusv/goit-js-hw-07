77797155b6bcec44fcc07f40e2e81f0934001e6e
Viktor Gorokhov <patronusv@gmail.com>
2020-11-25 21:50:44 +0200

    добавляет решение 5-го задания

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,10 @@
 'use strict';
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+let output = '';
+const inputToOutput = () => {
+  !inputRef.value || inputRef.value === '' ? (output = 'незнакомец') : (output = inputRef.value);
+  console.log(output);
+  outputRef.textContent = output;
+};
+inputRef.addEventListener('input', inputToOutput);
