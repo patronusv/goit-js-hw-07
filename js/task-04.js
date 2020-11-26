cb570fa368054d0abbba0acb52b0cd77835b5410
Viktor Gorokhov <patronusv@gmail.com>
2020-11-26 16:21:57 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,8 @@
 'use strict';
-let counterValue = 0;
 const counterRef = document.querySelector('#value');
+const btnDecrRef = document.querySelector('[data-action="decrement"]');
+const btnIncrRef = document.querySelector('[data-action="increment"]');
+let counterValue = 0;
 counterRef.textContent = counterValue;
 const decrement = () => {
   counterValue -= 1;
@@ -10,7 +12,5 @@ const increment = () => {
   counterValue += 1;
   counterRef.textContent = counterValue;
 };
-const btnDecrRef = document.querySelector('[data-action="decrement"]');
 btnDecrRef.addEventListener('click', decrement);
-const btnIncrRef = document.querySelector('[data-action="increment"]');
 btnIncrRef.addEventListener('click', increment);
