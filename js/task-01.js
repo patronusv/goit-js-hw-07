35ec1a2c8ef431d3bebeab8cad2e9fd876df7b00
Viktor Gorokhov <patronusv@gmail.com>
2020-11-26 16:19:07 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,7 @@
 'use strict';
 const categoriesRef = document.querySelector('#categories');
-// const categoriesCount = categoriesRef.children.length;
 console.log(`В списке ${categoriesRef.children.length} категории`);
-
 const itemRef = document.querySelectorAll('.item');
 itemRef.forEach(item => {
-  // const itemHeading = item.querySelector('h2').textContent;
-  // const itemCategoriesCount = item.querySelectorAll('li').length;
   console.log(`Категория: ${item.querySelector('h2').textContent}\nКоличество элементов: ${item.querySelectorAll('li').length}`);
 });
-// console.log(categories);
-// console.log(categoriesRef);
